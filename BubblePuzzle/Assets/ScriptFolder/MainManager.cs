a6a258f93915f7019c80eb8c62ef4293a4bb3e66
Yasmine Roy <y_oy@cda122ev5635x.local>
2018-02-14 08:27:25 -0500

    Added the Bubble Puzzle project
    Added the Bubble Puzzle project from where we last left off in class.

---
+++ BubblePuzzle/Assets/ScriptFolder/MainManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainManager : MonoBehaviour {
+
+	public int TotalScore;
+	public int TimeLeft = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+		StartCoroutine (CountDown ());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GameObject.Find ("ScoreLabel").GetComponent<Text> ().text = "Score: "+ TotalScore;
+
+		GameObject.Find ("TimeLabel").GetComponent<Text> ().text = "Time: "+ TimeLeft;
+
+		}
+
+
+	IEnumerator CountDown(){
+		while (TimeLeft > 0) {
+//			Debug.Log (TimeLeft);
+			yield return new WaitForSeconds (1);
+			TimeLeft--;
+
+			if (TimeLeft == 5) {
+
+				Debug.Log ("There are 5 seconds left!!");
+			}
+		}
+
+			
+
+
+		
+
+
+	}
+
+
+}
